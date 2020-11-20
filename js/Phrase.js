d43e83ced4c779d0191f4de3f3f58d62119753fb
JC <jcbrady89@yahoo.com>
2020-11-20 11:23:45 -0700

    Setup

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase()
+  }
+
+  /**
+   * adds letter placeholders to the display when the game starts.
+   */
+  addPhraseToDisplay() {}
+
+  /**
+   * checks to see if the letter selected by the player matches a letter in the phrase.
+   */
+  checkLetter() {}
+  /**
+   * reveals the letter(s) on the board that matches the player's selection. To reveal the matching letter(s), select all of the letter DOM elements that
+   * have a CSS class name that matches the selected letter and replace each selected
+   * element's hide CSS class with the show CSS class.
+   */
+  showMatchedLetter() {}
+} // end phrase class object
--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const phrase = new Phrase("Life is like a box of chocolates.")
+const game = new Game()
+
+//console.log(phrase)
+//console.log(phrase.phrase)
+//console.log(`Phrase - phrase: ${phrase.phrase}`)
+//console.log(game)
