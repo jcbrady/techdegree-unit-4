8bccf4800978b6cb51273b7f1e4aec1bf5561a75
JC <jcbrady89@yahoo.com>
2020-11-22 14:00:46 -0700

    Updated

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const phrase = new Phrase("Life is like chocolates.", "Want a free bowl of soup?", "Forest Gump meets E.T.")
+const phrase = new Phrase()
 const game = new Game()
 
-console.log(phrase)
-console.log(phrase.phrase)
-console.log(phrase.phrase1)
-console.log(`Phrase - phrase: ${phrase.phrase}`)
-console.log(game)
+// console.log(phrase)
+// console.log(phrase.phrase)
+// console.log(phrase.phrase1)
+// console.log(`Phrase - phrase: ${phrase.phrase}`)
+// console.log(game)
 
 console.log(game.phrases)
 
 game.phrases.forEach((phrase, index) => {
-  console.log(`Phrase ${index} - phrase: ${phrase.phrase}`)
+  console.log(`Phrase ${index} - phrase: ${phrase}`)
 })
